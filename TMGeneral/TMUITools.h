6662212df5ba96bf962e4f00ced9a3935c64b64c
KangKang <kang@thinkermobile.com>
2013-01-30 15:53:32 +0800

    f

--- TMGeneral/TMUITools.h
+++ TMGeneral/TMUITools.h
@@ -29,7 +29,7 @@ extern CGAffineTransform tmAspectFit(CGRect innerRect, CGRect outerRect);
  * scale
  The scale factor to apply to the bitmap. If you specify a value of 0.0, the scale factor is set to the scale factor of the device’s main screen.
  */
-extern extern UIImage *tmResizeImageWithScale(UIImage *aOriImage, CGSize aTargetSize, CGFloat aScale);
+extern UIImage *tmResizeImageWithScale(UIImage *aOriImage, CGSize aTargetSize, CGFloat aScale);
 
 /**
  * 這個沒有保護當寬度太小 連一個字都放不下去的時候的問題
